2adb3ad2fa46b7f8804a04b51d0578ba7e8ee6ff
sanjayaharshana <sanjayaharasana@gmail.com>
2022-02-18 00:08:00 +0530

    Nav Funcion Updated

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -1,5 +1,5 @@
     <div class="nav-bar">
-        <a href="#">
+        <a href="https://www.lankatiles.com">
             <img src="{{url('img/logo.png')}}" alt="brand-image">
         </a>
         <a href="https://www.lankatiles.com">
@@ -7,5 +7,5 @@
         </a>
     </div>
     <div class="hero-image">
-        <div  style="background-image: url('{{url('img/background.png')}}');height: 600px;background-position: center;background-repeat: no-repeat;background-size: cover;" alt="hero-image"></div>
+        <div  style="background-image: url('{{url('img/background.png')}}');height: 350px;background-position: center;background-repeat: no-repeat;background-size: cover;" alt="hero-image"></div>
     </div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,9 +11,20 @@
         {{csrf_field()}}
             <div class="price-checker">
                 <div class="title">Check our product prices (MRP)</div>
-                <input type="text" name="code" id="code" placeholder="Enter Tile Code"   oninput="this.value = this.value.toUpperCase()" required>
+                <input type="text" name="code" id="code" placeholder="Enter Tile Code" oninput="this.value = this.value.toUpperCase()" required>
                 <h5 class="mb-4" style="color:#909090; font-size:17px;">Example: FAEANW-M079A66</h5>
                 <button type="submit" class="btn-check">Check Now</button>
+
+                @if($price != 'Not Available')
+
+                @else
+                    <div class="row" style="height: 70px;"><div class="col-md-2" style="font-size: 40px;color: #ff0018;padding-top: 5px;text-align: left;margin-bottom: 20px;"><i class="bi bi-x-circle-fill"></i></div> <div class="col-md-10" style="text-align: left !important;"><div class="alert" style="font-size: 23px;color: rgb(255, 0, 0);margin-bottom: 0px;font-weight: 600;text-align: left;">Incorrect Tile Code</div></div></div>
+
+
+
+
+                @endif
+
             </div>
         </form>
 
@@ -25,10 +36,7 @@
                 <div class="price">LKR {{$price->mr_price_decimal}}</div>
             </div>
             @else
-            <div class="product-not-found">
-                <i class="bi bi-x-circle-fill"></i>
-                <div class="alert" style="font-size: 23px;color: #FF0000;margin-bottom: 0px;font-weight: 600">Incorrect Tile Code</div>
-            </div>
+
             @endif
         @endif
         </div>
